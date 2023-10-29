694793a28a4577a34decb1dad0a3b9cffeca493b
Kirill <lvovkir@yandex.ru>
2023-10-29 19:39:37 +0300

    Initial Commit

---
+++ SovkomBank/SovkomBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  SovkomBankApp.swift
+//  SovkomBank
+//
+//  Created by Kirill on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SovkomBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
