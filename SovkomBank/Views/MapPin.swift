aac3d250fc6a12b029cad774773adbb3dd7d0b05
Kirill <lvovkir@yandex.ru>
2023-11-05 18:44:41 +0300

    custom pins

---
+++ SovkomBank/Views/MapPin.swift
@@ -0,0 +1,20 @@
+//
+//  MapPin.swift
+//  SovkomBank
+//
+//  Created by Kirill on 05.11.2023.
+//
+
+import SwiftUI
+
+struct MapPin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapPin_Previews: PreviewProvider {
+    static var previews: some View {
+        MapPin()
+    }
+}
